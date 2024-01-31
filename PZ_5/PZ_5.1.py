bd731e617923779ff7213aa552929c5a02ed8805
ElenaMoskalenko <elenamoskalenko964@gmail.com>
2024-01-31 12:20:02 +0300

    ...

--- PZ_5/PZ_5.1.py
+++
@@ -1,7 +0,0 @@
-# Составить функцию, которая напечатает сорок любых символов.
-
-import random
-def print_random_chars():
-    chars = [chr(random.randint(33, 126)) for _ in range(40)]
-    print(''.join(chars))
-print_random_chars()
--- PZ_7/PZ_7.1.py
+++
@@ -1,8 +0,0 @@
-# Дано целое положительное число. Вывести символы, изображающие цифры этого
-# числа (в порядке слева направо).
-def print_digits(num):
-    digits = str(num)
-    for digit in digits:
-        print(digit)
-num = int(input("Введите целое положительное число: "))
-print_digits(num)
